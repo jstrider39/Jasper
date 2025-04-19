56621eac6e5faa41a5afac9d62e7a6fcbe37f5d7
jstrider39 <michaelSRoper@gmail.com>
2025-04-19 12:43:04 -0500

    init

---
+++ installs.md
@@ -0,0 +1,7 @@
+npm create vite@latest my-app -- --template react
+cd my-app
+npm install
+
+npm install @mui/material @emotion/react @emotion/styled
+
+npm install @mui/icons-material
---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { Button, Container, Typography } from "@mui/material";
+
+function App() {
+  return (
+    <Container sx={{ textAlign: "center", mt: 5 }}>
+      <Typography variant="h4" gutterBottom>
+        Welcome to My MUI App
+      </Typography>
+      <Button variant="contained" color="primary">
+        Click Me
+      </Button>
+    </Container>
+  );
+}
+
+export default App;
